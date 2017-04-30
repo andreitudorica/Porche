c68f4ade864e737286e2ecde21687d965a65cc00
Andrei <andreitudoricacj@gmail.com>
2017-04-30 15:13:20 +0300

    test

--- test/test/test.py
+++ test/test/test.py
@@ -1,3 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
+control = 3
+while(1):
+    GPIO.output(control,False)
+    time.sleep(2)
+    GPIO.output(control,True)
+    time.sleep(2)
