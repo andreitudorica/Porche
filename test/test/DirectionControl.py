4e63864e668924172450f062264403ce4aaeba2f
Andrei Tudorica <Andrei Tudorica>
2017-05-12 14:48:21 +0300

    throttle test from comp

--- test/test/DirectionControl.py
+++ test/test/DirectionControl.py
@@ -2,15 +2,15 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 TurningPin = 6
-TurningAmount = 7.5
+TurningAmount = 7.35
 GPIO.setup(TurningPin, GPIO.OUT)
 TurningPin = GPIO.PWM(6,50)
 TurningPin.start(TurningAmount)
 
-while 1:
-	TurningAmount = 7.35
-	TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
-	time.sleep(1)
-	TurningAmount = 8.59
-	TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
-	time.sleep(1)
+#while 1:
+#	TurningAmount = 7.35
+#	TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
+#	time.sleep(1)
+#	TurningAmount = 8.59
+#	TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
+#	time.sleep(1)
--- test/test/ThrottleControl.py
+++ test/test/ThrottleControl.py
@@ -1,20 +1,15 @@
+import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BCM)
+
 ThrottlePin = 5
 ThrottleAmount = 7.5
 GPIO.setup(ThrottlePin, GPIO.OUT)
-ThrottlePin = GPIO.PVM(5,50)
+ThrottlePin = GPIO.PwM(5,50)
 ThrottlePin.start(ThrottleAmount)
 
 
+ThrottleAmount = 7.8
+ThrottlePin.ChangeDutyCycle(ThrottleAmmount) #change Throttle
+time.sleep(1.5)
 ThrottleAmount = 7.5
-ThrottlePin.ChangeDutyCycle(ThrottleAmmount) #change Throttle
-time.sleep(1)
-ThrottleAmount = 9
-ThrottlePin.ChangeDutyCycle(ThrottleAmmount) #change Throttle
-time.sleep(1)
-ThrottleAmount = 7.5
-ThrottlePin.ChangeDutyCycle(ThrottleAmmount) #change Throttle
-time.sleep(1)
-ThrottleAmount = 9
-ThrottlePin.ChangeDutyCycle(ThrottleAmmount) #change Throttle
-time.sleep(1)
