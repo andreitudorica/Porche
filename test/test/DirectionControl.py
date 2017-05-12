a4ca00c9c4399a7dd5e30b887f4503db55f5bb84
andreitudorica <andreitudoricacj@gmail.com>
2017-05-12 11:44:09 +0000

    turning tested

--- test/test/DirectionControl.py
+++ test/test/DirectionControl.py
@@ -1,19 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
 TurningPin = 6
 TurningAmount = 7.5
 GPIO.setup(TurningPin, GPIO.OUT)
-TurningPin = GPIO.PVM(5,50)
+TurningPin = GPIO.PWM(6,50)
 TurningPin.start(TurningAmount)
 
-
-TurningAmount = 6.11
-TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
-time.sleep(1)
-TurningAmount = 8.89
-TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
-time.sleep(1)
-TurningAmount = 6.11
-TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
-time.sleep(1)
-TurningAmount = 8.89
-TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
-time.sleep(1)
+while 1:
+	TurningAmount = 7.35
+	TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
+	time.sleep(1)
+	TurningAmount = 8.59
+	TurningPin.ChangeDutyCycle(TurningAmount) #change Throttle
+	time.sleep(1)
