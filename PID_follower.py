382a0190984646de5601e8de9d3c4000c616f36f
AndreiBaraian <andreibaraian88@gmail.com>
2017-05-14 00:50:52 +0300

    implemented delta t

--- PID_follower.py
+++ PID_follower.py
@@ -2,10 +2,11 @@ from __future__ import division
 last_position = 0
 center_sensor=5
 nr_sensors=9
-Kp=15
+Kp=3
 Ki=1
-Kd=1#changed from 2
+Kd=0#changed from 2
 lerror = 0 #last error
+prevtime = 0 
 def calculate_error(position):
 
     return position -center_sensor
@@ -31,19 +32,22 @@ def  current_position(coded_position): #get the current position
         }
     return pos[coded_position]
 def set_last_error(error):
-    global lerror
     lerror = error
 
 def correction(raw_position):
 
+    currtime = time.time()
+    dt = currtime - prevtime
     position=current_position(raw_position)
     error=calculate_error(position)
-    motor_direction=Kp*error+Kd*(error-lerror)
+    if dt > 0:
+        motor_direction=Kp*error+Kd*(error-lerror) / dt
+    else:
+        motor_direction=Kp*error+Kd*(error-lerror)
     max_direction=Kp*4+4*Kd #compute the max rotation to normalize
     direction = motor_direction / max_direction
     set_last_error(error)
-
-
+    prevtime = currtime
 
     return direction
 
